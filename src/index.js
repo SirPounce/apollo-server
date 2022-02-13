223b4ebef8739ae5c8ca383d8c8e929bc08adf43
joiv <jonas.ivarsson@systecon.se>
2022-02-13 12:48:40 +0100

    Refactor cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import setupServer from "./server.js";
-import { config } from "dotenv";
+import { config } from 'dotenv';
+import server from './server';
 
 config(); // Setup envrironment variables
 
-setupServer()
-  .listen()
-  .then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+server()
+	.listen()
+	.then(({ url }) => {
+		console.log(`🚀 Server ready at ${url}`);
+	});
--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
-import { readFileSync } from "fs";
-import { Neo4jGraphQL } from "@neo4j/graphql";
-import { ApolloServer } from "apollo-server";
-import { driver as _driver, auth } from "neo4j-driver";
-import getEnv from "./environment.js";
+import { readFileSync } from 'fs';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { ApolloServer } from 'apollo-server';
+import { driver as _driver, auth } from 'neo4j-driver';
 
-const setupServer = () => {
-  const { uri, user, password } = getEnv();
+const server = () => {
+	const { NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD } = process.env;
 
-  const typeDefs = readFileSync(
-    new URL("./schema/schema.graphql", import.meta.url)
-  ).toString("utf-8");
+	const typeDefs = readFileSync(
+		new URL('./schema/schema.graphql', import.meta.url),
+	).toString('utf-8');
 
-  const driver = _driver(uri, auth.basic(user, password));
+	const driver = _driver(NEO4J_URI, auth.basic(NEO4J_USER, NEO4J_PASSWORD));
 
-  const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+	const { schema } = new Neo4jGraphQL({ typeDefs, driver });
 
-  return new ApolloServer({ schema: neoSchema.schema });
+	return new ApolloServer({ schema });
 };
 
-export default setupServer;
+export default server;
